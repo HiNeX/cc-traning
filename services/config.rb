d5852e0293a9fad1778ce17031273ec0bd81516f
Roman Hinex <roman@hinex.org>
2016-12-16 17:09:26 +0600

    Advise EC2 finalizing

--- services/config.rb
+++ services/config.rb
@@ -11,3 +11,9 @@ coreo_aws_advisor_alert "ec2-alert-snapshot" do
   operators [">", "=="]
   alert_when [8, false]
 end
+
+coreo_aws_advisor_ec2 "advise-ec2" do
+  action :advise
+  alerts ["ec2-alert-snapshot"]
+  regions ${AUDIT_AWS_ELB_REGIONS}
+end
