75ae9faeb46f8ecb28e92ff229e4c189b562e5ce
Roman Hinex <roman@hinex.org>
2016-12-16 11:45:24 +0600

    Bugfixing

--- services/config.rb
+++ services/config.rb
@@ -1,11 +1,13 @@
-coreo_aws_advistor_alert "alert_from_my_heart" do
+coreo_aws_advistor_alert "elb-inventory" do
   alert :define
   service :elb
   display_name "ELB Object Inventory"
-  description "Description"
+  description "Load balancer health check is greater then 2 min"
+  category "Inventory"
+  suggested_action "None."
   level "Informational"
   objectives ["load_balancers"]
-  audit_objects ["load_balancer_description.health_check.interval"]
+  audit_objects ["load_balancer_descriptions.health_check.interval"]
   operators [">"]
   alert_when [120]
 end
