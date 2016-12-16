a202e4f10edc1e0ef0d2cd28a5527bfc9f6c1bf9
Roman Hinex <roman@hinex.org>
2016-12-16 16:31:26 +0600

    EC2 snapshot

--- services/config.rb
+++ services/config.rb
@@ -12,6 +12,20 @@ coreo_aws_advisor_alert "cc-roman-training" do
   alert_when [120]
 end
 
+coreo_aws_advisor_alert "ec2-alert-snapshot" do
+  action :define
+  service :elb
+  display_name "ELB Object Health Check"
+  description "Load balancer health check"
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["describe_snapshots"]
+  audit_objects ["snapshot_set.volume_size", "snapshot_set.encrypted"]
+  operators [">", "==="]
+  alert_when [8, true]
+end
+
 coreo_aws_advisor_elb "cc-roman-training" do
   action :advise
   alerts ${ELB_ALERTS}
