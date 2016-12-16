475a930522895aeeb91a71ced12a5c0bbc94a3b8
Roman Hinex <roman@hinex.org>
2016-12-16 16:53:23 +0600

    EC2 clear code

--- services/config.rb
+++ services/config.rb
@@ -1,17 +1,3 @@
-coreo_aws_advisor_alert "cc-roman-training" do
-  action :define
-  service :elb
-  display_name "ELB Object Health Check"
-  description "Load balancer health check"
-  category "Inventory"
-  suggested_action "None."
-  level "Informational"
-  objectives ["load_balancers"]
-  audit_objects ["load_balancer_description.health_check.interval"]
-  operators [">"]
-  alert_when [120]
-end
-
 coreo_aws_advisor_alert "ec2-alert-snapshot" do
   action :define
   service :elb
@@ -24,10 +10,4 @@ coreo_aws_advisor_alert "ec2-alert-snapshot" do
   audit_objects ["snapshot_set.volume_size", "snapshot_set.encrypted"]
   operators [">", "=="]
   alert_when [8, false]
-end
-
-coreo_aws_advisor_elb "cc-roman-training" do
-  action :advise
-  alerts ${ELB_ALERTS}
-  regions ${ELB_REGIONS}
 end
