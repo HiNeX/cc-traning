996c30676d0782013dc5598a9efb62475bc79448
Roman Hinex <roman@hinex.org>
2016-12-16 12:27:29 +0600

    Update config yaml

--- services/config.rb
+++ services/config.rb
@@ -1,13 +1,18 @@
 coreo_aws_advistor_alert "elb-inventory" do
   action :define
   service :elb
-  display_name "ELB Object Inventory"
-  description "Load balancer health check is greater then 2 min"
+  display_name "ELB Object Health Check"
+  description "Load balancer health check"
   category "Inventory"
   suggested_action "None."
   level "Informational"
   objectives ["load_balancers"]
-  audit_objects ["load_balancer_descriptions.health_check.interval"]
+  audit_objects ["load_balancer_description.health_check.interval"]
   operators [">"]
   alert_when [120]
+end
+
+coreo_aws_advisor_elb "elb-inventory" do
+  action :advise
+  alerts ["roman"]
 end
